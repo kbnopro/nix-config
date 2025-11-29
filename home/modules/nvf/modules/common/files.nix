29a6bec2e0dabbbacf1158381c4918eee5d9482f
kbnopro <tuankhanhb00@gmail.com>
2025-11-29 12:23:55 +0700

    feat: oil for nvf

--- home/modules/nvf/modules/common/files.nix
+++ home/modules/nvf/modules/common/files.nix
@@ -6,13 +6,20 @@
       enable = true;
       setupOpts = {
         use_default_keymaps = false;
+        delete_to_trash = true;
+        keymaps = {
+          "<CR>" =  "actions.select";
+          "g." = { "@1" = "actions.toggle_hidden"; mode = "n"; };
+          "g\\" = { "@1" = "actions.toggle_trash"; mode = "n"; };
+          "-" = { "@1" = "actions.parent"; mode = "n"; };
+        };      
       };
     };
     vim.keymaps = [
       {
         key = "<leader>o";
         mode = ["n"];
-        action = ":Oil<CR>";
+        action = "<CMD>Oil<CR>";
         silent = true;
         desc = "Open Oil Explorer";
       }
