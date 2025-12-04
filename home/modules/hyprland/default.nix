4cefb7330e3f6d54a282e6a05ade8b3dde12d95b
kbnopro <tuankhanhb00@gmail.com>
2025-12-04 22:24:39 +0700

    feat: keybinds for brightness

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -1,13 +1,22 @@
 {
-  mylib, ... 
+  mylib, config, lib, pkgs,  ... 
 }:
 
-{
+with lib;
+
+let
+  cfg = config.wayland.windowManager.hyprland;
+in {
   imports = mylib.scanPaths ./.;
 
-  wayland.windowManager.hyprland = {
-    package = null;
-    portalPackage = null;
+  config = mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      package = null;
+      portalPackage = null;
+    };
+    home.packages = with pkgs; [
+      brightnessctl
+    ];
   };
 }
 
--- home/modules/hyprland/keybinds.nix
+++ home/modules/hyprland/keybinds.nix
@@ -1,3 +1,8 @@
+# Inspired by: https://github.com/end-4/dots-hyprland/blob/main/.config/hypr/hyprland/keybinds.conf
+# We will have special syntax to be able to parse this for cheat sheet (in the future I hope)
+# Lines ending with `# [hidden]` won't be shown on cheatsheet
+# Lines starting with #! and #!! are section headings and sub headings
+# Lines starting with #/# are only for cheatsheet
 {
   ...
 }:
@@ -5,7 +10,22 @@
 {
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "SUPER, T, exec, kitty"
+      "SUPER, T, exec, kitty" # Temporary terminal
+
+    ];
+
+    bindle = [
+      ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+" # [hidden]
+      ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-" # [hidden]
+      ", XF86MonBrightnessUp, exec, brightnessctl set '5%+'"
+      ", XF86MonBrightnessDown, exec, brightnessctl set --min-value=1 '5%-'"
+    ];
+
+    bindl = [
+      ",XF86AudioPlay, exec, playerctl play-pause" # [hidden]
+      ",XF86AudioPause, exec, playerctl play-pause" # [hidden]
+      ",XF86AudioNext, exec, playerctl next" # [hidden]
+      ",XF86AudioPrev, exec, playerctl previous" # [hidden]
     ];
   };
 }
