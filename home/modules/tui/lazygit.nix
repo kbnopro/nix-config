79512d895af6ba526019f5317be51da5d0028229
kbnopro <tuankhanhb00@gmail.com>
2025-12-24 00:07:35 +0700

    feat: color for lazygit

--- home/modules/tui/lazygit.nix
+++ home/modules/tui/lazygit.nix
@@ -1,9 +1,43 @@
 {
+  config,
   ...
 }:
-
+let
+  color = config.background.color;
+in
 {
   programs.lazygit = {
     enable = true;
+    settings = {
+      gui.theme = {
+        activeBorderColor = [
+          "${color.primary}"
+          "bold"
+        ];
+        selectedLineBgColor = [
+          "${color.secondaryContainer}"
+        ];
+        searchingActiveBorderColor = [
+          "${color.term6}" # cyan
+          "bold"
+        ];
+        optionsTextColor = [
+          "${color.term4}" # blue
+        ];
+        cherryPickedCommitFgColor = [
+          "${color.onPrimaryContainer}"
+        ];
+        cherryPickedCommitBgColor = [
+          "${color.primaryContainer}"
+        ];
+        # doesn't seem to work
+        # markedBaseCommitFgColor = [
+        #   "red"
+        # ];
+        # markedBaseCommitBgColor = [
+        #   "red"
+        # ];
+      };
+    };
   };
 }
