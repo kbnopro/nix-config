57dbdea5cb4150392e113a952ee4facf0442dd1b
kbnopro <tuankhanhb00@gmail.com>
2026-03-08 12:33:26 +1000

    fix: wl-clipboard installed on darwin

--- nvf/modules/common/config/options.nix
+++ nvf/modules/common/config/options.nix
@@ -1,32 +1,42 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
-  config.vim.clipboard = {
-    enable = true;
-    registers = "unnamedplus";
+  config = lib.mkMerge [
+    (lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+      vim.clipboard.providers = {
+        wl-copy.enable = true;
+      };
+    })
 
-    providers = {
-      wl-copy.enable = true;
-    };
-  };
+    {
+      vim.clipboard = {
+        enable = true;
+        registers = "unnamedplus";
+      };
+    }
+    {
 
-  config.vim.options = {
-    # indent stuffs
-    autoindent = true;
-    shiftwidth = 2;
-    tabstop = 2;
-    expandtab = true;
-    smartindent = true;
+      vim.options = {
+        # indent stuffs
+        autoindent = true;
+        shiftwidth = 2;
+        tabstop = 2;
+        expandtab = true;
+        smartindent = true;
 
-    termguicolors = true; # true color
+        termguicolors = true; # true color
 
-    timeoutlen = 1000; # wait to finish sequence
-    ttimeoutlen = 0;
+        timeoutlen = 1000; # wait to finish sequence
+        ttimeoutlen = 0;
 
-    wrap = true;
-    linebreak = true;
-    wrapmargin = 0;
-    textwidth = 0;
+        wrap = true;
+        linebreak = true;
+        wrapmargin = 0;
+        textwidth = 0;
 
-    undofile = true;
-  };
+        undofile = true;
+      };
+    }
+
+  ];
+
 }
