cc441a3f28a3c3cc3cd405cf05a72237a793fe4b
kbnopro <tuankhanhb00@gmail.com>
2026-03-08 13:21:05 +1100

    fix: hyprcursor theme always installed

--- home/modules/hyprland/env.nix
+++ home/modules/hyprland/env.nix
@@ -1,15 +1,22 @@
 {
   pkgs,
+  config,
+  lib,
   ...
 }:
+let
+  cfg = config.wayland.windowManager.hyprland;
+in
 
 {
-  wayland.windowManager.hyprland.settings = {
-    env = [
-      "HYPRCURSOR_THEME,rose-pine-hyprcursor"
+  config = lib.mkIf cfg.enable {
+    wayland.windowManager.hyprland.settings = {
+      env = [
+        "HYPRCURSOR_THEME,rose-pine-hyprcursor"
+      ];
+    };
+    home.packages = with pkgs; [
+      rose-pine-hyprcursor
     ];
   };
-  home.packages = with pkgs; [
-    rose-pine-hyprcursor
-  ];
 }
