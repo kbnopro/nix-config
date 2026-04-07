ba51d9eb2dd62e2eb0826d6bb1686be8810e113f
kbnopro <tuankhanhb00@gmail.com>
2026-04-07 17:53:03 +1000

    feat(nvf): git browse

---
+++ modules/nvf/modules/common/snacks/gitbrowse.nix
@@ -0,0 +1,33 @@
+{ ... }:
+{
+  vim.keymaps = [
+    {
+      key = "<leader>gB";
+      action = ''
+        function() 
+          Snacks.gitbrowse()
+        end
+      '';
+      desc = "Git Browse (Copy)";
+      lua = true;
+      mode = [
+        "n"
+        "x"
+      ];
+    }
+    {
+      key = "<leader>gY";
+      action = ''
+        function() 
+          Snacks.gitbrowse({open = function(url) vim.fn.setreg('+', url) end, notify = false})
+        end
+      '';
+      desc = "Git Browse (Copy)";
+      lua = true;
+      mode = [
+        "n"
+        "x"
+      ];
+    }
+  ];
+}
