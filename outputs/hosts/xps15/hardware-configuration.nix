230d6c22582c78c68ea1995b6b3051ec94477d01
kbnopro <tuankhanhb00@gmail.com>
2026-07-02 19:04:02 +0700

    fix: hardware changed

--- outputs/hosts/xps15/hardware-configuration.nix
+++ outputs/hosts/xps15/hardware-configuration.nix
@@ -25,15 +25,8 @@
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/627285c2-2922-4146-aff0-04cc56d52fd6"; }
+    [ { device = "/dev/disk/by-uuid/504cca04-cf48-4820-af11-5d4b93ad34db"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
