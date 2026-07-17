5c2c3e2c73b9e674537fff70aea40843a7cd9ca8
kbnopro <tuankhanhb00@gmail.com>
2026-07-17 16:48:51 +0700

    feat: firewall, disable openssh port

--- outputs/hosts/meshy/configuration.nix
+++ outputs/hosts/meshy/configuration.nix
@@ -69,6 +69,8 @@
     shell = pkgs.fish;
   };
 
+  networking.firewall.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -113,9 +115,6 @@
   # };
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
